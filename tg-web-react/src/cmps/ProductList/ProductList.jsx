233b0530b7201222f9bc568977ad6f29e0c6e8ca
MKLSV <mkolosov96@gmail.com>
2025-06-30 02:42:48 +0300

    ss

--- tg-web-react/src/cmps/ProductList/ProductList.jsx
+++ tg-web-react/src/cmps/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import ProductItem from '../ProductItem/ProductItem';
 import { useTelegram } from '../../hooks/useTelegram';
 import './ProductList.css'
