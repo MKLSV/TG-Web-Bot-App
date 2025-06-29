a1fd8c20f6ef97b4a616d0cc5c66f8d662ce8502
Matvey Kolosov <118886359+MKLSV@users.noreply.github.com>
2025-06-30 02:23:08 +0300

    Add files via upload

---
+++ tg-web-react/src/hooks/useTelegram.js
@@ -0,0 +1,26 @@
+
+export function useTelegram() {
+    
+    const tg = window.Telegram.WebApp
+    
+    const onClose = () => {
+        tg.close()
+    }
+
+
+    const onToggleButton = () => {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide()
+        } else{
+            tg.MainButton.show()
+        }
+    }
+
+    return {
+        onClose,
+        tg,
+        user: tg.initDataUnsafe?.user,
+        onToggleButton,
+        queryId: tg.initDataUnsafe?.query_id
+    }
+}
